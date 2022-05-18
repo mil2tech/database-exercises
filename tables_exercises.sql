09bc8a9a06d63ab42a168519076bee62765cbbda
Jarrid Jones <jarid.d.jones@gmail.com>
2022-05-18 17:02:09 -0500

    added table_exercises with line commands

---
+++ tables_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+SHOW tables; # This will show tables for selected database #
+#### There are 5 columns and they use different data types###
+Describe employees; ###This will show character inputs for each column###
+
+# What different data types are present on this table? #
+# The inputs that are utilized are numbers(integers), strings (letters and or alphanumeric), date and enumberated. ##
+
+# Which table(s) do you think contain a numeric type column?#
+# Emp_no#
+
+#Which table(s) do you think contain a string type column? #
+# first_name and last_name#
+
+#Which table(s) do you think contain a date type column?#
+# birth_date and hire_date#
+
+#What is the relationship between the employees and the departments tables?#
+# the table dept_emp relates both tables#
+
+# This reveals the SQL for table#
+SHOW CREATE Table dept_manager
+
+
+
