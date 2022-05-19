24f7f35489b91c80299d06d2722aa6fd1f4fb6a7
Jarrid Jones <jarid.d.jones@gmail.com>
2022-05-19 10:49:02 -0500

    added more command lines to complete exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,25 @@
 USE albums_db;
 
+SHOW TABLES;
+
+DESCRIBE albums;
+
+SHOW CREATE TABLE albums;
+
+SELECT * FROM albums;
+#How many rows are in the albums table? 31 rows
+
+SELECT DISTINCT artist
+FROM albums;
+#How many unique artist names are in the albums table? 23 artists
+
+##What is the primary key for the albums table?
+
+
+#What is the oldest release date for any album in the albums table? What is the most recent release date?
+
+
+
+
+
 
